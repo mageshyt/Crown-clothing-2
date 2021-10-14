d7c6d2fe744b55954cecb4dd427469bd960c7007
magesh <hariarunengg@gmail.com>
2021-10-14 12:42:37 +0530

    first commit

---
+++ src/components/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Banner = () => {
+  return (
+    <div>
+      {/* Image */}
+      <div className="banner-container w-full h-96"></div>
+    </div>
+  );
+};
+a;
---
+++ src/components/Home/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between h-14 bg-gray-200">
+      {/* Logo */}
+      <div className="logo-image ml-4">
+        <img src="/images/crown.svg" alt="logo " />
+      </div>
+      {/* header section */}
+      <div className="sections font-bold flex flex-row space-x-8">
+        <h2>Home</h2>
+        <h2>Shop</h2>
+        <h2>pages</h2>
+        <h2>Contact</h2>
+      </div>
+      {/* login and logo */}
+      <div className="login-section flex flex-row space-x-2 mr-4">
+        <h2 className="font-bold btn btn-md ">Sign up</h2>
+      </div>
+    </div>
+  );
+};
+export default Header;
